32f7f0f0b2bd4b85129ecd02b83c82b5ac1f3766
Paul Ramirez <paulr1604@gmail.com>
2022-10-07 01:45:05 -0500

    ajuste de usuario capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :deploy_to, "/home/apps/qhalifarma"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/webpacker', 'vendor', '.bundle', 'public/system', 'public/uploads', 'storage'
+#append :linked_files, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/webpacker', 'vendor', '.bundle', 'public/system', 'public/uploads', 'storage'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
