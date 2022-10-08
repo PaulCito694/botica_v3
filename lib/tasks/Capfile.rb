d8144bdf4371d0e2191b3b9221386c92e8e9def2
Paul Ramirez <paulr1604@gmail.com>
2022-10-08 16:52:57 -0500

    ajuste de usuario capistrano

--- lib/tasks/Capfile.rb
+++
@@ -1,8 +0,0 @@
-require 'capistrano/bundler'
-require 'capistrano/passenger'
-require 'capistrano/rails/assets'
-require 'capistrano/rails/migrations'
-require 'capistrano/rbenv'
-
-set :rbenv_type, :user
-set :rbenv_ruby, '2.7.1'
